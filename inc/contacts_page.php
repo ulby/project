ccc7e85840881bec0cc2d52c4eebf8b922c06907
ulby <j.ulb@me.com>
2014-10-01 14:53:03 +0200

    Update contacts_page.php

--- inc/contacts_page.php
+++ inc/contacts_page.php
@@ -1,4 +1,18 @@
 <?php
+if(!isset($_POST["save"])) $_POST["save"] = "";
+if($_POST["save"] == "Save") {
+	if( !empty($_POST["name"]) &&
+	!empty($_POST["email"]) &&
+	!empty($_POST["message"]) &&
+	!empty($_POST["date"])) {
+		mysql_query("INSERT into contacts (Name,Email,Message,Date) values ('$_POST[name]','$_POST[email]','$_POST[message]','$_POST[date]')");
+	} else {
+		echo "Please fill in all information.";
+	}
+}
+if(isset($_GET['del'])) {
+	mysql_query("DELETE from contacts where ID = '$_GET[del]'");
+}  
 if(!isset($_POST["savebutton"])) $_POST["savebutton"] = "";
 if($_POST["savebutton"] == "Save") {
 	if( !empty($_POST["Name"]) && 
@@ -10,25 +24,8 @@ if($_POST["savebutton"] == "Save") {
 		echo "Please fill in all information.";
 	}
 }
-$w = mysql_query("select * from contacts where Name='New contact'");
-if (mysql_num_rows($w) == 0) { 
-	mysql_query("insert into contacts (Name) values ('New contact')");
-} 
 ?>
-
-		<form method="POST" action="admin.php?site=contacts">
-         <select name="aktcontact">
-             <?php
-             if(!isset($_POST["aktcontact"])) $_POST["aktcontact"] = "";
-             $contacts = mysql_query("select Name from contacts order by ID asc");
-             while($ds = mysql_fetch_assoc($contacts)) {
-                 echo "<option value='$ds[Name]'";
-                 if ($ds["Name"] == $_POST["aktcontact"]) echo " selected";
-                 echo">$ds[Name]</option>";
-             }
-             ?>
-         </select>
-         <input class="input" type="submit" value="GO">
+<a href="?site=contacts&edit=new">ADD NEW</a>
 <?php
 echo "<table>";
 $contactslist = mysql_query("select * from contacts");
@@ -38,21 +35,36 @@ while($stream = mysql_fetch_assoc($contactslist)) {
 	echo "<td>$stream[Email] </td>";
 	echo "<td>$stream[Message] </td>";
 	echo "<td>$stream[Date]</td>";
+	echo "<td><a href='?site=contacts&edit=$stream[ID]'>EDIT</a></td>";
+	echo "<td><a href='?site=contacts&del=$stream[ID]'>DELETE</a></td>";
 	echo "<tr>";
 }
 echo "</table>";
 
 ?>
-
+<form method="POST" action="?site=contacts">
 <?php
-if($_POST['aktcontact'] == "New contact") {
-$contacts = mysql_query("select * from contacts where Name ='$_POST[aktcontact]' order by Name ASC");
+if(isset($_GET['edit'])) {
+	if($_GET['edit'] == "new") {
+		?>
+		<table>
+			<tr>
+				<td><input name="name" value="" placeholder="Name"></td>
+				<td><input type="email" name="email" value="" placeholder="Email"></td>
+				<td><textarea name="message" value="" placeholder="Message"></textarea></td>
+				<td><input type="date" name="date"></td>
+				<td><input type="submit" name="save" value="Save"></td>
+			</tr>
+		</table>
+		</form>
+		<?php	
+	} else {
+$contacts = mysql_query("select * from contacts where ID ='$_GET[edit]'");
 while($zeile=mysql_fetch_assoc($contacts)) {
 	$key = $zeile["ID"];
 	$data[$key] = $zeile;
 }
-mysql_close($db_link);
-
+mysql_close();
 echo"<table cellspacing=0 >";
         echo "<tr>";
         foreach($data[$key] as $datenfeld => $wert) {
@@ -76,7 +88,7 @@ echo"<table cellspacing=0 >";
             echo "</tr>";
             }
         echo"</table>";
-}
 ?>
 <input type="submit" value="Save" name="savebutton">
 </form>
+<?php }} ?>
